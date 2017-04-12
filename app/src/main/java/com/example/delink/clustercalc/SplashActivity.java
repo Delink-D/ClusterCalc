e64855b6e46a25c9c557b1512931f261b26ec92f
Delink-D <delinkdesigns@gmail.com>
2017-04-12 14:19:13 +0300

    Added thes plash screen
    Made changes to the splash appbar theme to no appbar
    set the spalsh screen iamge which is the logo and welcoming app name with custom font family

---
+++ app/src/main/java/com/example/delink/clustercalc/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.delink.clustercalc;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Bind(R.id.splash_title) TextView mSplashTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ButterKnife.bind(this);
+
+        Typeface archistico = Typeface.createFromAsset(getAssets(), "fonts/Archistico_Bold.ttf");
+        mSplashTitle.setTypeface(archistico);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
